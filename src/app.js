95530e33ac9b445d1f8319805777a7653be9ffa7
Debajyoti mandal <150023795+debu000199@users.noreply.github.com>
2026-03-20 16:22:46 +0530

    Allow all origins in CORS configuration

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:8080', 'https://eshop.devsubhadipbag.in'],
+    origin: ['http://localhost:8080', 'https://eshop.devsubhadipbag.in', "*"],
     credentials: true,
   })
 );
